587fe9c2d87f91dc51101abfb0dee66ced5d8c7b
yasmin.haidar <yasmin.haidar@justeattakeaway.com>
2025-08-03 23:28:11 +0100

    created deleteTodo func

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
 	app.Patch("/api/todos/:id", updateTodos)
-	// app.Delete("/api/todos/:id", deleteTodos)
+	app.Delete("/api/todos/:id", deleteTodos)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -128,4 +128,17 @@ func updateTodos(c *fiber.Ctx) error {
 
 }
 
-// func deleteTodos(c *fiber.Ctx) error {}
+func deleteTodos(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "invalid Id"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
